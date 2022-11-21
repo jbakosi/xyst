84a14c6ce117028d3597a3643cc9d73707d01001
Jozsef Bakosi <jbakosi@protonmail.com>
2022-11-21 11:18:22 +0000

    Update copyright in tools/

--- tools/extract_ctr_keywords.sh
+++ tools/extract_ctr_keywords.sh
@@ -8,6 +8,7 @@
 # \copyright 2012-2015 J. Bakosi,
 #            2016-2018 Los Alamos National Security, LLC.,
 #            2019-2021 Triad National Security, LLC.
+#            2022-2023 J. Bakosi
 #            All rights reserved. See the LICENSE file for details.
 # \details   This script runs an executable and extracts the help for each of
 # the executable's control file keyword, describing their documentation,
--- tools/update_copyright.sh
+++ tools/update_copyright.sh
@@ -6,13 +6,10 @@
 # \copyright 2012-2015 J. Bakosi,
 #            2016-2018 Los Alamos National Security, LLC.,
 #            2019-2021 Triad National Security, LLC.
+#            2022-2023 J. Bakosi
 #            All rights reserved. See the LICENSE file for details.
 ################################################################################
 
-# Suggested argument directories:
-#
-#   quinoa: doc tests tools src
-#   quinoa-tpl: docker
-#   quinoa-cmake: .
+# Suggested argument directories: cmake tests tools src
 
-find doc -type f -not -name update_copyright.sh -exec sed -i 's/2019-2020 Triad National Security, LLC./2019-2021 Triad National Security, LLC./' {} +
+find cmake -type f -not -name update_copyright.sh -exec sed -i 's/2019-2021 Triad National Security, LLC./2019-2021 Triad National Security, LLC.\n#            2022-2023 J. Bakosi/' {} +

df522ef279eb916cbba18fbdf6920a8b9a9c8eaf
Jozsef Bakosi <jbakosi@protonmail.com>
2022-11-21 11:18:03 +0000

    Update copyright in tests/

--- tests/unit/Control/TestSystemComponents.cpp
+++ tests/unit/Control/TestSystemComponents.cpp
@@ -4,6 +4,7 @@
   \copyright 2012-2015 J. Bakosi,
              2016-2018 Los Alamos National Security, LLC.,
              2019-2021 Triad National Security, LLC.
+#            2022-2023 J. Bakosi
              All rights reserved. See the LICENSE file for details.
   \brief     Unit tests for Control/SystemComponents
   \details   Unit tests for Control/SystemComponents

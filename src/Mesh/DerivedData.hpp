034130ae40796a5981c7180d212175c4ccbee716
Jozsef Bakosi <jbakosi@protonmail.com>
2023-03-10 16:21:47 +0000

    Remove unused code

--- src/Mesh/DerivedData.hpp
+++ src/Mesh/DerivedData.hpp
@@ -183,24 +183,6 @@ intet( const std::array< std::vector< real >, 3 >& coord,
        std::size_t e,
        std::array< real, 4 >& N );
 
-//! Compute curl of a vector field at nodes of unstructured tetrahedra mesh
-tk::UnsMesh::Coords
-curl( const std::array< std::vector< tk::real >, 3 >& coord,
-      const std::vector< std::size_t >& inpoel,
-      const tk::UnsMesh::Coords& v );
-
-//! Compute divergence of vector field at nodes of unstructured tetrahedra mesh
-std::vector< tk::real >
-div( const std::array< std::vector< tk::real >, 3 >& coord,
-     const std::vector< std::size_t >& inpoel,
-     const tk::UnsMesh::Coords& v );
-
-//! Compute gradient of a scalar field at nodes of unstructured tetrahedra mesh
-tk::UnsMesh::Coords
-grad( const std::array< std::vector< tk::real >, 3 >& coord,
-      const std::vector< std::size_t >& inpoel,
-      const std::vector< tk::real >& phi );
-
 } // tk::
 
 #endif // DerivedData_h

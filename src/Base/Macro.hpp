edc2659e2c073e412707e1a0e04c047d8eff692c
Jozsef Bakosi <jbakosi@protonmail.com>
2023-06-16 12:02:19 +0100

    Fix types in timing macros

--- src/Base/Macro.hpp
+++ src/Base/Macro.hpp
@@ -23,15 +23,15 @@ namespace tk {
 //! the section of code to be profiled.
 #define STARTTIME \
 struct timeval START_TIME, END_TIME; \
-int total_usecs; \
-gettimeofday(&START_TIME, (struct timezone*)0);
+long total_usecs; \
+gettimeofday( &START_TIME, NULL );
 
 //! End-time macro for fine-grained profiling. Put this at the end of the
 //! section of code to be profiled.
 #define ENDTIME \
-gettimeofday(&END_TIME, (struct timezone*)0); \
+gettimeofday( &END_TIME, NULL ); \
 total_usecs = (END_TIME.tv_sec-START_TIME.tv_sec) * 1000000 + (END_TIME.tv_usec-START_TIME.tv_usec); \
-printf("Total time was %d uSec.\n", total_usecs);
+printf("Total time was %ld uSec.\n", total_usecs);
 
 //! Macro to detect strictly gcc.
 //! \details __GNUC__ and __GNUG__ were intended to indicate the GNU compilers.

c869fb80e94575c630e8ee56ec90b60b660aef11
Jozsef Bakosi <jbakosi@protonmail.com>
2023-11-02 18:48:06 +0000

    Echo solver selected in input file

--- src/Control/InciterConfig.cpp
+++ src/Control/InciterConfig.cpp
@@ -896,6 +896,7 @@ Config::control()
     get< tag::rescomp >() = unsigint( L, "rescomp", 1, true );
     get< tag::part >() = string( L, "part", "rcb", true );
     get< tag::solver >() = string( L, "solver", "riecg", true );
+    print << "Solver: " << get< tag::solver >() << '\n';
     get< tag::fct >() = boolean( L, "fct", true, true );
 
     ic( L, *this );

1ec2f6a22b063cb41021e9b298ba9b1e3cb52794
AliSaleemHasan <ali1salem1hasan@gmail.com>
2025-04-15 23:27:32 +0200

    Outputing scalar fields on surfaces in Chocg solver

--- src/Inciter/ChoCG.cpp
+++ src/Inciter/ChoCG.cpp
@@ -1928,6 +1928,10 @@ ChoCG::writeFields( CkCallback cb )
     nodesurfnames.push_back( "velocityz" );
     nodesurfnames.push_back( "divergence" );
     nodesurfnames.push_back( "pressure" );
+    
+    for (std::size_t c=0; c<ncomp-3 ; ++c) {
+      nodesurfnames.push_back( "c" + std::to_string(c) );
+    }
 
     auto bnode = tk::bfacenodes( m_bface, m_triinpoel );
     std::set< int > outsets( begin(f), end(f) );
@@ -1947,6 +1951,7 @@ ChoCG::writeFields( CkCallback cb )
         nodesurfs[i+(p++)][j] = s[2];
         nodesurfs[i+(p++)][j] = m_div[n];
         nodesurfs[i+(p++)][j] = m_pr[n];
+ 	for (std::size_t c=0; c<ncomp-3; ++c) nodesurfs[i+(p++)+c][j]= s[3+c];
         ++j;
       }
     }
